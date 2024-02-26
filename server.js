d7fdd2ef97b887d23b3139ac6bcf3713ab58de91
Marco Nocito <mnocito@mit.edu>
2024-02-26 10:00:21 -0500

    post

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
  var express = require('express');
  var serveStatic = require('serve-static');
  var cors = require('cors');
+ const fileUpload = require('express-fileupload');
+ 
  var app = express()
  app
      .use(cors())
+     .use(fileUpload())
      .use(serveStatic(__dirname))
      .listen(3000, () => console.log('Server running on 3000...'));
 
-app.post('/', (req, res) => {
+ app.post('/file', (req, res) => {
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+      }
+    
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+    
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
+      if (err)
+        return res.status(500).send(err);   
+        res.send('File uploaded!');
+      });
     res.send('POST request to the homepage')
 })
